fb444b5b79524eefbe9a52639a8c6aa8071b9537
Mark Koh <markkohdev@gmail.com>
2016-05-31 20:32:56 -0400

    Commiting some basic stuffs.

---
+++ idea.md
@@ -0,0 +1,5 @@
+## CS480 Final Project
+### Idea - Image to Music
+For this project, we will use the Python Libraries PyAudio and OpenCV, to perform an image analysis on a user-decided image, and from the image generate a song.  Some ideas that we have for ways to compile this song from an image are to create notes which correspond to different colors, then generate a song from that.  Another idea is to take R/G/B and interperate it as Pitch/Duration/Amplitude.
+There are many possibile ways to implement this idea, and we won't know what works and doesn't work until we try it, so that's the main plan :)
+
---
+++ musicjawn.py
@@ -0,0 +1,48 @@
+import argparse
+from PIL import Image
+
+# Our Constant jawns
+# PIXEL_BEATVAL = 1.0/8.0
+PIXEL_REPEAT_THRESHOLD = 2
+
+# We gotta parse this ish
+parser = argparse.ArgumentParser(description='Take an image and produce a music from it!')
+parser.add_argument('image', help='The input image!')
+
+parser.add_argument('output_file', help='The output audio file (.wav)')
+
+args = parser.parse_args()
+
+
+print "Input: {}, Output: {}".format(args.image, args.output_file)
+
+# Open the image file and read the RGB pixel values into an array
+im = Image.open(args.image, 'r')
+width, height = im.size
+pixel_values = list(im.getdata())
+
+
+# import ipdb; ipdb.set_trace()
+
+
+prevs = [0,0,0]
+prev_lengths = [0,0,0]
+values = [[],[],[]]
+
+for pixel in pixel_values:
+    for v in range(0,3):
+        if prevs[v] == pixel[v]:
+            # If this pixel value for the color is equal to
+            # the last color, increment the count
+            prev_lengths[v] += 1
+        else:
+            # Otherwise, store the conut and reset the value
+            store = (prevs[v],prev_lengths[v])
+            values[v].append(store)
+            prevs[v] = pixel[v]
+            prev_lengths[v] = 0
+
+
+# print values
+import ipdb; ipdb.set_trace()
+
